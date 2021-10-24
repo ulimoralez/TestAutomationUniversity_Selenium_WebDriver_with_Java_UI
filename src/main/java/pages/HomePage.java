4aa6a5d551671066d936ec0bf894aaafba9ee75a
Ulises Moralez <moralezulises@gmail.com>
2021-10-24 19:14:05 -0300

    Chapter 5 - Advanced Interactions

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -26,6 +26,10 @@ public class HomePage {
         return new ForgotPasswordPage(driver);
     }
 
+    public HoversPage clickHovers(){
+        clickLink("Hovers");
+        return new HoversPage(driver);
+    }
     private void clickLink(String linkText){
         driver.findElement(By.linkText(linkText)).click();
     }
